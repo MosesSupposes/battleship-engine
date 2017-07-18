79429c0e07786fdff5fcb5c8f3118e540ae0dcbe
Moses Samuel <mosamuel98@gmail.com>
2017-07-18 01:08:08 -0700

    add takeTurn and checkGameStatus methods + tests

---
+++ test/gameTest.js
@@ -0,0 +1,42 @@
+var expect = require('chai').expect;
+
+describe('GAME INSTANCE FUNCTIONS', function () {
+    describe('checkGameStatus', function () {
+        var checkGameStatus = require('../game_logic/gameInstance.js').checkGameStatus;
+        it('should tell me when the game is over', function () {
+        var players = [
+            {
+                ships: [
+                    {
+                        locations: [[0, 0]],
+                        damage: [[0, 0]]
+                    }
+                ]
+            }
+        ];
+        var actual = checkGameStatus(players);
+        expect(actual).to.be.false;
+        });
+    });
+
+    describe('takeTurn', function() {
+        var takeTurn = require('../game_logic/gameInstance').takeTurn;
+        var guess, player;
+
+        beforeEach(function() {
+            guess = () => [0, 0];
+            player = {
+                ships: [
+                    {
+                        locations: [[0, 0]], damage: []
+                    }
+                ]
+            }
+        });
+
+        it('should return false if the game ends', function() {
+            var actual = takeTurn(player, guess);
+            expect(actual).to.be.false;
+        });
+    });
+});
